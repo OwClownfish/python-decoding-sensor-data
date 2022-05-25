605d5722675f8256fa73d3960f8165e67a63a275
Hielke <Hielke@DESKTOP-F14N2B0>
2022-05-25 16:12:05 +0200

    aaa

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -6,7 +6,7 @@ def load_sensor_data():
 	sensor_data = []
 	sensor_files = glob.glob(os.path.join(os.getcwd(),"datasets","*.csv"))
 	for sensor_file in sensor_files:
-		with(open(sensor_file) as data_file):
+		with open(sensor_file) as data_file:
 			data_reader = csv.DictReader(sensor_file,delimiter=",")
 			for row in data_file:
 				sensor_data.append(row)
