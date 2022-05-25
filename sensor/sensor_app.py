e961cdc8f2d8d198fd2e09fa65a5f5a1daf0784b
Hielke <Hielke@DESKTOP-F14N2B0>
2022-05-25 16:47:39 +0200

    aaaa

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,7 @@
 # Runner script for all modules
 from load_data import load_sensor_data
 from house_info import HouseInfo
+from datetime import date, datetime
 
 ##############################
 # Do not remove these two lines
@@ -17,8 +18,10 @@ print("Loaded records: {}".format(len(data)))
 house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area("id" , rec_area=test_area)
-
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+
+test_date = datetime.strptime("5/9/20","%m/%d/%y")
+print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 3 code here:
 
 # Module 4 code here:
